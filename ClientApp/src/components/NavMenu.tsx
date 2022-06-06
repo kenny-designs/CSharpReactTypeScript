0eb3f6f697e9ae7032e9cc72a816621dba715ea4
Alexander M. Aguilar <alexanderman2004@yahoo.com>
2022-06-05 22:17:10 -0500

    Added @types/react-router-dom via npm

--- ClientApp/src/components/NavMenu.tsx
+++ ClientApp/src/components/NavMenu.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
 export const NavMenu = () => <h1>Please work...</h1>;
